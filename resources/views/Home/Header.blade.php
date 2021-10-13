f9593504ca0ed25115660cbb9d0936347fa167c7
kennoshino <kennoshino@gmail.com>
2021-10-13 10:35:47 +0700

    header

--- resources/views/Home/Header.blade.php
+++ resources/views/Home/Header.blade.php
@@ -1,4 +1,34 @@
-<section id="menu" class="menu">
-    sajsjhas
+<section id="header" class="header">
+    
+    {{-- top header --}}
+    <div class="top-header">
+        <div class="left-top-header">
+            <div class="connection-header">Kết nối :
+                {{-- hình ảnh của facebook, intagram, ... và link --}}
+            </div>
+        </div>
+
+        <div class="top-header-spacer"></div>
+
+        <div class="right-top-header">
+            
+            <div class="notification-top-header header_link">
+                <a href=""><i class="bi bi-bell"></i>Thông Báo</a>
+            </div>
+
+            <div class="register-top-header header_link">
+                <a href="">Đăng ký</a>
+            </div>
+
+            <div class="login-top-header header_link">
+                <a href="">Đăng nhập</a>
+            </div>
+
+        </div>
+    </div>
+    {{-- bottom header --}}
+    <div class="bottom-header">
+        
+    </div>
 </section>
 
