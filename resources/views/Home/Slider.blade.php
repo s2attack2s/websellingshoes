eb02a22fc0c381c4ee8105aeeb574198bb27d8ff
Nguyen Trong Son <s2attack2s@gmail.com>
2021-10-14 11:04:58 +0700

    responsive

--- resources/views/Home/Slider.blade.php
+++ resources/views/Home/Slider.blade.php
@@ -1,10 +1,8 @@
 <!-- Section Slider -->
-<section id="slider" class="slider">
-    <div class="container">
+<section id="slider" class="slider">  
         <div class="slider-nav">
             <img class="service-img" src="https://capvirgo.com/wp-content/uploads/2017/12/1111111111111banner-nike-tong.jpg" />
             <img class="service-img" src="https://capvirgo.com/wp-content/uploads/2017/12/1111111111111banner-nike-tong.jpg" />
         </div>
-    </div>
 </section>
 <!--End Section Slider -->
